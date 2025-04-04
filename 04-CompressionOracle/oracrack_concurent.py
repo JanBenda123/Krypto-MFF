b1b72cc61a9a0c8b7c45c78907995db9603cb3be
Jan Benda <benda.honza1@gmail.com>
2025-04-04 10:17:16 +0200

    refactoring

--- 04-CompressionOracle/oracrack_concurent.py
+++ 04-CompressionOracle/oracrack_concurent.py
@@ -3,38 +3,40 @@ import random
 from concurrent.futures import ThreadPoolExecutor
 
 
-alphabeth = "QWERTZUIOPASDFGHJKLYXCVBNM1234567890_}{?!.#@&$*"
+alphabeth = "QWERTZUIOPASDFGHJKLYXCVBNM1234567890_}{?!.#@&$*" # used alphabetth
+flag = "KRYPTO"
 
-def generate_fill(N):
+guess_repeats = 15 
+fill_size = 8
+rounds = 20
+
+def generate_fill(N): 
     return ''.join(random.choices(alphabeth, k=N))
 
 def send(payload):
-    return requests.post("http://krypto.ptera.cz/depressed_oracle.php", data={"plaintext": payload[1]})
+    return requests.post("http://krypto.ptera.cz/depressed_oracle.php", data={"plaintext": payload})
 
-def question_round(executor):
+def generate_payloads():
+    # random fills are the same for all payloads to minimize the influence of randomness
+    # payloads differ only on the position of chars currently guessed
     first_fill = generate_fill(fill_size)
     payloads = [first_fill for c in alphabeth]
-    for _ in range(repeats):
-        fill = generate_fill(fill_size)
+    for _ in range(guess_repeats):
+        fill = generate_fill(fill_size)                                     
         payloads = [ p+ flag+c+fill for p,c in zip(payloads,alphabeth)]
+    return payloads
 
-
+def question_round(executor):
+    payloads = generate_payloads()
     candidates =[]
     
-    responses = list(executor.map(send, payloads))
+    responses = list(executor.map(send, payloads)) # runs send() for each payload in parallel using threads/processes
     for response in responses:
         candidates.append(len(response.text))
     return candidates
 
-
-flag = "KRYPTO"
-
-repeats = 10
-fill_size = 16
-
-rounds = 20
-
-while True:
+def get_candidates(rounds):
+    # runs the same set of request multiple times ()
     round_results = []
     with ThreadPoolExecutor(max_workers=len(alphabeth)) as executor:
         for _ in range(rounds):
@@ -49,6 +51,13 @@ while True:
 
     candidates=list(zip(alphabeth,candidates))
     candidates.sort(key=lambda x:x[1])
+    return candidates
+
+
+
+
+while True:
+    candidates = get_candidates(rounds)
 
     print("current flag guess is: ", flag)
     print("the best candidates are:")
@@ -64,7 +73,7 @@ while True:
     if choice == ".":
         continue
     if choice.startswith("-r "):
-        repeats = int(choice[3:])
+        guess_repeats = int(choice[3:])
         continue
     if choice.startswith("-fs "):
         fill_size = int(choice[4:])
